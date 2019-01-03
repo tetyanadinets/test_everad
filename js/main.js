35c035f6931b3c69313115e261fa4cb20d49b158
Tetyana Dinets <tetyanadinets@gmail.com>
2019-01-03 19:54:46 +0200

    fix picture height

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 jQuery(document).ready(function() {
 	new WOW().init();
+	setEqualHeight(jQuery(".px-advantages__img"));
+});
+
+jQuery(window).resize(function() {
+  setEqualHeight(jQuery(".px-advantages__img"));
 });
 
 jQuery(window).scroll(function(){
-    if ($(this).scrollTop() > 78) {
-        jQuery('.px-header').addClass('px-header_active');
-    } else {
-        jQuery('.px-header').removeClass('px-header_active');
-    }
+	if ($(this).scrollTop() > 78) {
+		jQuery('.px-header').addClass('px-header_active');
+	} else {
+		jQuery('.px-header').removeClass('px-header_active');
+	}
 });
+
+function setEqualHeight(columns) {
+	var tallestcolumn = 0;
+	columns.each(
+		function() {
+			jQuery(this).css('height', 'auto');
+			currentHeight = jQuery(this).height();
+			if (currentHeight > tallestcolumn) {
+				tallestcolumn = currentHeight;
+			}
+		}
+	);
+	columns.height(tallestcolumn);
+}
